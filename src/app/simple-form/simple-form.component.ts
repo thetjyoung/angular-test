ca9f1dabc540f0ff5065372c9b9d0882a16e9827
spencerr9 <spencer.ricks9@gmail.com>
2023-05-17 21:13:42 -0600

    Integrate the title service Display values of the title service on the form

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
+import { TitleService } from '../title.service';
 
 @Component({
   selector: 'app-simple-form',
   templateUrl: './simple-form.component.html',
-  styleUrls: ['./simple-form.component.sass']
+  styleUrls: ['./simple-form.component.sass'],
+  providers: [TitleService]
 })
 export class SimpleFormComponent {
+name: any;
+  constructor(private service: TitleService) {}
+  
+  titlesArr: any = []
 
+  ngOnInit(): void {
+    this.service.getTitles()
+      .subscribe((val: any) => this.titlesArr = val)
+  }
 }
