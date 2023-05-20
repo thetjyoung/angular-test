d3b0b2cff90cec8a9138eb308616637be35a1b70
spencerr9 <spencer.ricks9@gmail.com>
2023-05-19 18:04:21 -0600

    Output form submissions

--- src/app/simple-form/simple-form.component.ts
+++ src/app/simple-form/simple-form.component.ts
@@ -8,20 +8,43 @@ import { TitleService } from '../title.service';
   providers: [TitleService]
 })
 export class SimpleFormComponent {
-name: any;
-  constructor(private service: TitleService) {}
-  
-  titlesArr: any = [];
+
+  firstName: string = ""
+  lastName: string = ""
+  termsConds: boolean = false
+  selectedTitle: any;
+  titlesArr: any;
+  filteredTitles: any;
   specialCharacters: any = /[~`!#$%\^&*+=\-\[\]\\';,/{}|\\":<>\?]/g
 
+  constructor(private service: TitleService) {}
+  
   ngOnInit(): void {
+    // Make titles available to IOC container
     this.service.getTitles()
       .subscribe((val: any) => this.titlesArr = val)
     
     // Remove "!" (also removes titles with any of the characters in the `specialCharacters` variable above)
-    this.titlesArr = this.titlesArr.filter((title: any) => !this.specialCharacters.test(title.name))
+    this.filteredTitles = this.titlesArr.filter((title: any) => !this.specialCharacters.test(title.name))
 
     // Sort Alphabetically
-    this.titlesArr.sort((a: any,b: any) => a.name > b.name ? 1 : -1)
+    this.filteredTitles.sort((a: any,b: any) => a.name > b.name ? 1 : -1)
+
+    // Define the default title
+    this.selectedTitle = this.filteredTitles.find((el: any) => el.isDefault === true).name
+  }
+  
+  // Keep selectedTitle up to date based on user input
+  onSelected(value: any): void {
+    this.selectedTitle = value
+  }
+  
+  printFormValues(): void {
+    console.log({
+      title: this.selectedTitle,
+      firstName: this.firstName,
+      lastName: this.lastName,
+      acceptTerms: this.termsConds
+    })
   }
 }
